252d3f11e4b2a311613afe0f65c67229a7a89996
Artem Grankin <artem_grankin@epam.com>
2021-01-12 18:12:39 +0300

    feature: update styles

--- client/src/components/SideMenu/styled-components.js
+++ client/src/components/SideMenu/styled-components.js
@@ -1,9 +1,12 @@
 import styled, { keyframes } from "styled-components";
 import { fadeInLeft, fadeIn } from 'react-animations';
 import { IoIosArrowDropright, IoIosCloseCircleOutline } from 'react-icons/io';
-import { mdSize, smSize } from "../../css-сonstants/screen-breakpoints";
+import { smSize, mdSize, lgSize } from "../../css-сonstants/screen-breakpoints";
+import { maroonX11, ghostWhite} from "../../css-сonstants/colors";
+import { sideMenuBigWidth, sideMenuSmallWidth} from "../../css-сonstants/dimensions";
+import { materialShadow2 } from "../../css-сonstants/shadows";
 
-const wrapperWidth = '15rem'
+
 const buttonWidth = '2.5rem'
 const wrapperPadding = '1rem'
 
@@ -13,32 +16,41 @@ const  Wrapper = styled.div`
   top: 0;
   bottom: 0;
   left: 0;
-  width: ${wrapperWidth};
   padding: ${wrapperPadding};
-  border-right: 1px solid black;
-  z-index: 1;
-  animation: ${keyframes(fadeInLeft)} .3s ease-in-out;
-
   display: flex;
   flex-direction: column;
   align-items: center;
-
+  z-index: 1;
+  animation: ${keyframes(fadeInLeft)} .3s ease-in-out;
+  color: ${ghostWhite};
+  background-color: ${maroonX11};
+  box-shadow: inset ${materialShadow2};
+  
   & > h1 { 
     font-size: 1.2rem;
     word-wrap: break-word; 
   }
 
   @media only screen and (max-width: ${smSize}) {
+    width: ${sideMenuBigWidth};
     &::after {
       content: '';
       position: fixed;
       top: 0;
       right: 0;
       bottom: 0;
-      left: ${wrapperWidth};
+      left: ${sideMenuBigWidth};
       background-color: rgba(0, 0, 0, .8);
       animation: ${keyframes(fadeIn)} .7s ease-in-out;
     }
+  }
+
+  @media only screen and (min-width: ${smSize}) {
+    width: ${sideMenuSmallWidth};
+  }
+
+  @media only screen and (min-width: ${lgSize}) {
+    width: ${sideMenuBigWidth};
   }
 `
 
@@ -94,7 +106,6 @@ const OpeningIcon = styled(IoIosArrowDropright)`
 const Content = styled.div`
   flex-grow: 1;
   width: 100%;
-  border: 1px solid black;
 `
 
 export {
