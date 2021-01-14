38aed0bc0a3b4e5e311641bf250a3c6a522c4d27
Artem Grankin <artem_grankin@epam.com>
2021-01-14 18:45:37 +0300

    fix: add new param

--- processing/validations/createTextChecker.js
+++ processing/validations/createTextChecker.js
@@ -4,8 +4,9 @@ exports.createTextChecker = (params) => {
   const min = params.min || 0
   const max = params.max || 300
   const optional = params.optional || false
+  const fieldName = params.fieldName || 'text'
 
-  const checker = check('text').trim().isLength({ min, max })
+  const checker = check(fieldName).trim().isLength({ min, max })
 
   return optional ? checker.optional() : checker
 }
