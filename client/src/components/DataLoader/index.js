aa6a2d2f41776d3bf2c0b4800e1c9db452fa3381
Artem Grankin <artem_grankin@epam.com>
2021-01-18 18:52:40 +0300

    fix: fix condition for res

--- client/src/components/DataLoader/index.js
+++ client/src/components/DataLoader/index.js
@@ -1,43 +1,40 @@
 import React from 'react'
- 
+
 class DataLoader extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
       data: null,
-      error: null
+      error: null,
     }
   }
 
   componentDidMount() {
-    const headers =  { 'Content-Type': 'application/json' }
+    const headers = { 'Content-Type': 'application/json' }
     fetch(this.props.url, { headers })
-      .then(res => {
-        if (!res.status.ok) {
+      .then((res) => {
+        if (!res.ok) {
           throw new Error(`Data retrieval error. Status: ${res.status}`)
         }
         return res.json()
       })
-      .then(result => {
+      .then((result) => {
         this.setState({
-          data: result
+          data: result,
         })
       })
-      .catch(error => {
+      .catch((error) => {
         this.setState({
-          error
+          error,
         })
       })
   }
 
   render() {
-    const {error, data} = this.state
+    const { error, data } = this.state
 
-    return (
-      this.props.children(data, error)
-    )
+    return this.props.children(data, error)
   }
 }
-
 
 export default DataLoader
