8364798006c7bd21804a9a928538fa3d7c49564a
Artem Grankin <artem_grankin@epam.com>
2021-01-18 18:09:55 +0300

    chore: move to router into a separate component

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,7 @@
-import { Switch, Route, Link, Redirect, BrowserRouter } from 'react-router-dom'
-
-import NavSideMenu from './components/NavSideMenu'
-import NotesPage from './pages/NotesPage'
-import GroupsPage from './pages/GroupsPage'
+import AppRouter from './AppRouter'
 
 function App() {
-  return (
-    <BrowserRouter>
-      <NavSideMenu>
-        <Link to="/notes">Notes</Link>
-        <Link to="/goups">Groups</Link>
-      </NavSideMenu>
-
-      <Switch>
-        <Route path="/" exact>
-          <Redirect to="/notes" />
-        </Route>
-        <Route path="/notes">
-          <NotesPage />
-        </Route>
-        <Route path="/goups">
-          <GroupsPage />
-        </Route>
-      </Switch>
-    </BrowserRouter>
-  )
+  return <AppRouter />
 }
 
 export default App
---
+++ client/src/AppRouter.js
@@ -0,0 +1,30 @@
+import { Switch, Route, Link, Redirect, BrowserRouter } from 'react-router-dom'
+
+import NavSideMenu from './components/NavSideMenu'
+import NotesPage from './pages/NotesPage'
+import GroupsPage from './pages/GroupsPage'
+
+function AppRouter() {
+  return (
+    <BrowserRouter>
+      <NavSideMenu>
+        <Link to="/notes">Notes</Link>
+        <Link to="/goups">Groups</Link>
+      </NavSideMenu>
+
+      <Switch>
+        <Route path="/" exact>
+          <Redirect to="/notes" />
+        </Route>
+        <Route path="/notes">
+          <NotesPage />
+        </Route>
+        <Route path="/goups">
+          <GroupsPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  )
+}
+
+export default AppRouter
