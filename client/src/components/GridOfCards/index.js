363b5ccbb9ae2c18c3055c79f8786231a91e4970
Artem Grankin <artem_grankin@epam.com>
2021-01-20 19:43:04 +0300

    feature: add grid for cards

---
+++ client/src/components/GridOfCards/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { Wrapper, Grid } from './styled-components'
+import DataLoadingComponent from '../DataLoadingComponent'
+import generateRanges from './utils'
+import CardGroup from '../CardGroup'
+
+class GridOfCards extends React.Component {
+  render() {
+    const { cardContent, loadingLimit: limit } = this.props
+    const url = `api/${cardContent}/length`
+
+    return (
+      <>
+        <Wrapper>
+          <DataLoadingComponent {...{ url }}>
+            {(data) => {
+              const ranges = generateRanges(data.count, limit)
+
+              return (
+                <Grid>
+                  {ranges.map((range, i) => (
+                    <CardGroup key={i} {...{ range, limit, cardContent }} />
+                  ))}
+                </Grid>
+              )
+            }}
+          </DataLoadingComponent>
+        </Wrapper>
+      </>
+    )
+  }
+}
+
+GridOfCards.propTypes = {
+  loadingLimit: PropTypes.number.isRequired,
+  cardContent: PropTypes.oneOf(['groups', 'notes']).isRequired,
+}
+
+export default GridOfCards
---
+++ client/src/components/GridOfCards/styled-components.js
@@ -0,0 +1,21 @@
+import styled from 'styled-components'
+
+const Wrapper = styled.div`
+  height: 100%;
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`
+
+const Grid = styled.div`
+  box-sizing: border-box;
+  display: grid;
+  grid: auto-flow minmax(auto, 30%) / repeat(auto-fit, minmax(300px, 1fr));
+  gap: 1rem;
+  padding: 1rem;
+  height: 100%;
+  width: 100%;
+`
+
+export { Wrapper, Grid }
