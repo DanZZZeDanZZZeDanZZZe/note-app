5d53302a12ace97b0e6799b0f364128ee46e71b5
Artem Grankin <artem_grankin@epam.com>
2021-01-24 12:30:12 +0300

    feature: add click handler for card

---
+++ client/src/pages/NotesInGroupPage/index.js
@@ -0,0 +1,19 @@
+import { useParams } from 'react-router-dom'
+import GridOfCards from '../../components/GridOfCards'
+import PageWrapper from '../../components/PageWrapper'
+
+function NotesInGroupPage() {
+  const { groupTitle } = useParams()
+  return (
+    <PageWrapper>
+      <GridOfCards
+        routeForContent="groups/notes"
+        cardType="notes"
+        loadingLimit={5}
+        param={groupTitle}
+      />
+    </PageWrapper>
+  )
+}
+
+export default NotesInGroupPage
