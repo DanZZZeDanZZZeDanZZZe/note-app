d89fad515b93ab26943273594855f8683267b8f2
Artem Grankin <artem_grankin@epam.com>
2021-01-26 07:12:06 +0300

    feature: add note addition page

--- client/src/pages/AddNotePage/index.js
+++ client/src/pages/AddNotePage/index.js
@@ -1,7 +1,12 @@
+import AddNoteForm from '../../components/AddNoteForm'
 import PageWrapper from '../../components/PageWrapper'
 
 function AddNotePage() {
-  return <PageWrapper>add note</PageWrapper>
+  return (
+    <PageWrapper>
+      <AddNoteForm />
+    </PageWrapper>
+  )
 }
 
 export default AddNotePage
