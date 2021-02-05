ce5b1bad8b99afbc1b5d8a0bfa531770da1da8cb
Artem Grankin <artem_grankin@epam.com>
2021-02-05 18:04:01 +0300

    chore: add redux dev-tools support

--- client/src/state/store.js
+++ client/src/state/store.js
@@ -1,11 +1,15 @@
 import { applyMiddleware, createStore } from 'redux'
 import createSagaMiddleware from 'redux-saga'
 
+import { composeWithDevTools } from 'redux-devtools-extension'
 import rootReducer from './reducers/rootReducer'
 import rootSaga from './sagas/rootSaga'
 
 const sagaMiddleware = createSagaMiddleware()
-const store = createStore(rootReducer, applyMiddleware(sagaMiddleware))
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware))
+)
 
 sagaMiddleware.run(rootSaga)
 
